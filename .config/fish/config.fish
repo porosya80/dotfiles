dbd3a3491f61be3ec3d5bf04dd7c6efa1ad5429e
Dmytro Myk <porosya@gmail.com>
2024-10-28 01:11:52 +0200

    fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,12 @@ set -gx GPG_TTY (tty)
 set -gx PATH $PATH ~/.local/bin
 if status is-interactive
   starship init fish | source
-  zoxide init --cmd cd fish | source
   atuin init fish | source
+  zoxide init --cmd cd fish | source
 # Commands to run in interactive sessions can go here
-end
 if not set -q SSH_CONNECTION
   fish_ssh_agent
 end
 
+end
 
